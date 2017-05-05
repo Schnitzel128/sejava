6f751d4df910e3474b5a4f65e79d98b2e9f767fb
Schnitzel128 <schnitzel128@gmail.com>
2017-05-05 11:50:52 +0200

    Programm Update Style

--- eclipse-ws-impl/GradleDezHex/src/main/java/dezhextextfield/DezHexTextField.java
+++ eclipse-ws-impl/GradleDezHex/src/main/java/dezhextextfield/DezHexTextField.java
@@ -3,18 +3,31 @@ import javafx.scene.control.TextField;
 
 public class DezHexTextField extends TextField
 {
-	@Override public void replaceText(int start, int end, String text) {
-        if (text.matches("[0-9,A-F]") || text == "") {
+	@Override public void replaceText(int start, int end, String text) 
+	{
+        if (text.matches("[0-9,A-F]") || text == "") 
+        {
             super.replaceText(start, end, text);
+            verify();
         }
     }
   
-    @Override public void replaceSelection(String text) {
-        if (text.matches("[0-9,A-F]") || text == "") {
+    @Override public void replaceSelection(String text) 
+    {
+        if (text.matches("[0-9,A-F]") || text == "") 
+        {
             super.replaceSelection(text);
+            verify();
         }
+    }
+    
+    private void verify() {
+        if (getText().length() > 10) {
+            setText(getText().substring(0, 10));
+        }
+
     }
 }
 
 //einbinden mit DezHexTextField und
-//<?import testpackage.DezHexTextField?>
+//<?import dezhextextfield.DezHexTextField?>
