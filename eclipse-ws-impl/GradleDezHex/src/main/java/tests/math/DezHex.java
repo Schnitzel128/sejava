f358e5e91f4d298266370aef44fae8b12c0962ab
Bolkun <serhij16@live.de>
2017-05-11 09:48:00 +0200

    Tests in Projekt integrieren

---
+++ eclipse-ws-impl/GradleDezHex/src/main/java/tests/math/DezHex.java
@@ -0,0 +1,21 @@
+package tests.math;
+
+public class DezHex {
+	
+	public static String toHex(String zahl) throws Exception{
+		if(Long.parseLong(zahl)<0 || Long.parseLong(zahl)>Long.MAX_VALUE)
+            throw new Exception();
+		//System.out.println(Long.parseLong(zahl));
+		String s = Long.toHexString(Long.parseLong(zahl));
+		//System.out.println(s);
+		return s;		
+	}
+	
+	public static String toDez(String zahl) throws Exception{
+		if(Long.valueOf(zahl,16).longValue()>Long.MAX_VALUE)
+			throw new Exception();
+		String s = Long.toString(Long.valueOf(zahl,16).longValue());
+		//System.out.println(s);
+		return s;
+	}
+}
---
+++ eclipse-ws-impl/GradleDezHex/src/main/java/tests/testDezHex.java
@@ -0,0 +1,41 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import tests.math.DezHex;
+
+public class testDezHex {
+	
+	@Test
+	public void testeToHex() throws Exception{
+		String zahl = "436547342";
+		String result = DezHex.toHex(zahl);
+		assertEquals("1a052f0e", result);
+	}
+	
+	@Test
+	public void testeToDez() throws Exception{
+		String zahl = "AAAFFB";
+		String result = DezHex.toDez(zahl);
+		assertEquals("11186171", result);
+		//kleinbuchstabige übergabe
+		String zahl2 = "aaaffb";
+		String result2 = DezHex.toDez(zahl2);
+		assertEquals("11186171", result2);
+	}
+	
+	@Test(expected=Exception.class)
+	public void testeToHexAufUeberlauf() throws Exception{
+		long z = Long.MAX_VALUE+100;
+		String zahl = Long.toString(z);
+		String result = DezHex.toHex(zahl);
+	}
+	
+	@Test(expected=Exception.class)
+	public void testeToDezAufUeberlauf() throws Exception{
+		String zahl = "AAAFFBAAAAAAAAAA";
+		String result = DezHex.toDez(zahl);
+	}
+	
+}
